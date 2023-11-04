17ea422d61da7d4cb02d0e6741e2fd851c0d402c
guscooke <guscooke14@gmail.com>
2023-11-04 09:54:32 -0400

    testing VScode

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,5 +1,8 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda"
 
+// This func will be invoked by "APIGATEWAY"_AUTHORIZER_CHANGE_DEPLOYMENT_LOGICAL_ID, 
+// Need to pass APIGATEWAYEVENT and Returns to client APIGProxyREsult
+
 export async function handler(event: APIGatewayProxyEvent, 
     context: Context): Promise<APIGatewayProxyResult>{
 
