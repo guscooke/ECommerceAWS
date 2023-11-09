a038b9763fa587bfba0693c0e6809873cfa42db3
guscooke <guscooke14@gmail.com>
2023-11-09 09:24:50 -0300

    1-Stacks-Created

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -5,9 +5,12 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, 
     context: Context): Promise<APIGatewayProxyResult>{
+        
+        const lambdaRequestId = context.awsRequestId
+        const apiRequestId = event.requestContext.requestId
 
+        console.log(`API Gateway requestId: ${apiRequestId} - lambda RequestId ${lambdaRequestId} `)
         const method = event.httpMethod
-
         if (event.resource === "/products"){
             if (method === 'GET'){
                 console.log('GET')
@@ -31,3 +34,4 @@ export async function handler(event: APIGatewayProxyEvent,
 
 
 
+// geração de log consome cpuUsage, gera Custos e somos cobrados por tempo de execução lambda
